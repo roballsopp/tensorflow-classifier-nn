b12881c34b37e0fb8aca0d83494b1cc43a8a0ddf
Rob Allsopp <rca06d@my.fsu.edu>
2017-01-28 15:10:41 -0700

    remove semi-colons to make pycharm happy

--- nn.py
+++ nn.py
@@ -1,68 +1,68 @@
 import tensorflow as tf
 
 def randInitializeWeights(numInputs, numOutputs):
-	epsilon = 0.12;
+	epsilon = 0.12
 	return tf.random_uniform([numOutputs, numInputs], minval=-epsilon, maxval=epsilon, dtype=tf.float32)
 
 def forward_prop(a1, Theta1, bias1, Theta2, bias2):
-	z2 = tf.matmul(a1, tf.transpose(Theta1)) + bias1;
-	a2 = tf.sigmoid(z2);
-	z3 = tf.matmul(a2, tf.transpose(Theta2)) + bias2;
-	return tf.sigmoid(z3);
+	z2 = tf.matmul(a1, tf.transpose(Theta1)) + bias1
+	a2 = tf.sigmoid(z2)
+	z3 = tf.matmul(a2, tf.transpose(Theta2)) + bias2
+	return tf.sigmoid(z3)
 
 def cost(X, y, Theta1, bias1, Theta2, bias2):
-	y_num = tf.cast(y, tf.float32);
-	m = tf.to_float(tf.shape(X)[0]);
-	a3 = forward_prop(X, Theta1, bias1, Theta2, bias2);
-	y1 = -y_num * tf.log(a3);
-	y0 = (1 - y_num) * tf.log(1 - a3);
+	y_num = tf.cast(y, tf.float32)
+	m = tf.to_float(tf.shape(X)[0])
+	a3 = forward_prop(X, Theta1, bias1, Theta2, bias2)
+	y1 = -y_num * tf.log(a3)
+	y0 = (1 - y_num) * tf.log(1 - a3)
 	return tf.reduce_sum((y1 - y0) / m)
 
 def regTerm(Theta1, Theta2, m, lam):
-	regTheta1 = tf.reduce_sum(Theta1 ** 2);
-	regTheta2 = tf.reduce_sum(Theta2 ** 2);
-	regTerm = lam / (2*m) * (regTheta1 + regTheta2);
+	regTheta1 = tf.reduce_sum(Theta1 ** 2)
+	regTheta2 = tf.reduce_sum(Theta2 ** 2)
+	regTerm = lam / (2*m) * (regTheta1 + regTheta2)
 
 def evaluate(X_val, y_val, Theta1, bias1, Theta2, bias2):
-	y_hyp = forward_prop(X_val, Theta1, bias1, Theta2, bias2);
+	y_hyp = forward_prop(X_val, Theta1, bias1, Theta2, bias2)
 
-	correct_labels = tf.cast(y_val, tf.bool);
-	predicted_labels = tf.cast(tf.round(y_hyp), tf.bool);
+	correct_labels = tf.cast(y_val, tf.bool)
+	predicted_labels = tf.cast(tf.round(y_hyp), tf.bool)
 
-	correct_prediction = tf.equal(correct_labels, predicted_labels);
+	correct_prediction = tf.equal(correct_labels, predicted_labels)
 
-	true_pos = tf.logical_and(correct_labels, predicted_labels);
-	false_pos = tf.logical_and(tf.logical_not(correct_labels), predicted_labels);
-	true_neg = tf.logical_and(tf.logical_not(correct_labels), tf.logical_not(predicted_labels));
-	false_neg = tf.logical_and(correct_labels, tf.logical_not(predicted_labels));
+	true_pos = tf.logical_and(correct_labels, predicted_labels)
+	false_pos = tf.logical_and(tf.logical_not(correct_labels), predicted_labels)
+	true_neg = tf.logical_and(tf.logical_not(correct_labels), tf.logical_not(predicted_labels))
+	false_neg = tf.logical_and(correct_labels, tf.logical_not(predicted_labels))
 
-	accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32));
+	accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-	num_true_pos = tf.reduce_sum(tf.cast(true_pos, tf.float32));
-	num_false_pos = tf.reduce_sum(tf.cast(false_pos, tf.float32));
-	num_true_neg = tf.reduce_sum(tf.cast(true_neg, tf.float32));
-	num_false_neg = tf.reduce_sum(tf.cast(false_neg, tf.float32));
+	num_true_pos = tf.reduce_sum(tf.cast(true_pos, tf.float32))
+	num_false_pos = tf.reduce_sum(tf.cast(false_pos, tf.float32))
+	num_true_neg = tf.reduce_sum(tf.cast(true_neg, tf.float32))
+	num_false_neg = tf.reduce_sum(tf.cast(false_neg, tf.float32))
 
-	tf.summary.scalar('num_true_pos', num_true_pos);
-	tf.summary.scalar('num_false_pos', num_false_pos);
-	tf.summary.scalar('num_true_neg', num_true_neg);
-	tf.summary.scalar('num_false_neg', num_false_neg);
+	tf.summary.scalar('num_true_pos', num_true_pos)
+	tf.summary.scalar('num_false_pos', num_false_pos)
+	tf.summary.scalar('num_true_neg', num_true_neg)
+	tf.summary.scalar('num_false_neg', num_false_neg)
 
-	num_total_pos = tf.reduce_sum(tf.cast(predicted_labels, tf.float32));
+	num_total_pos = tf.reduce_sum(tf.cast(predicted_labels, tf.float32))
 	
-	tf.summary.scalar('num_total_pos', num_total_pos);
+	tf.summary.scalar('num_total_pos', num_total_pos)
 
-	get_precision = lambda: (num_true_pos / num_total_pos);
-	get_recall = lambda: (num_true_pos / (num_true_pos + num_false_neg));
-	get_zero = lambda: tf.constant(0, dtype=tf.float32);
+	get_precision = lambda: (num_true_pos / num_total_pos)
+	get_recall = lambda: (num_true_pos / (num_true_pos + num_false_neg))
+	get_zero = lambda: tf.constant(0, dtype=tf.float32)
 
-	precision = tf.cond(tf.cast(num_total_pos, tf.bool), get_precision, get_zero);
-	recall = tf.cond(tf.cast((num_true_pos + num_false_neg), tf.bool), get_recall, get_zero);
+	precision = tf.cond(tf.cast(num_total_pos, tf.bool), get_precision, get_zero)
+	recall = tf.cond(tf.cast((num_true_pos + num_false_neg), tf.bool), get_recall, get_zero)
 
-	get_f1_number = lambda: (2 * precision * recall / (precision + recall));
+	get_f1_number = lambda: (2 * precision * recall / (precision + recall))
 
-	f1 = tf.cond(tf.cast((precision + recall), tf.bool), get_f1_number, get_zero);
+	f1 = tf.cond(tf.cast((precision + recall), tf.bool), get_f1_number, get_zero)
 
-	return [accuracy, precision, recall, f1];
+	return [accuracy, precision, recall, f1]
 
-# J = (sum(sum(y1 - y0)) / m) + regTerm;
+# J = (sum(sum(y1 - y0)) / m) + regTerm
--- train.py
+++ train.py
@@ -2,10 +2,10 @@ import tensorflow as tf
 import nn
 
 def train(layers, data, folder = 'run1'):
-	input_layer_size, hidden_layer_size, num_labels = layers;
-	X, y, X_val, y_val = data;
+	input_layer_size, hidden_layer_size, num_labels = layers
+	X, y, X_val, y_val = data
 
-	graph = tf.Graph();
+	graph = tf.Graph()
 	with graph.as_default():
 		X_placeholder = tf.placeholder(tf.float32, shape=(None, input_layer_size), name='X')
 		y_placeholder = tf.placeholder(tf.uint8, shape=(None, num_labels), name='y')
@@ -18,29 +18,29 @@ def train(layers, data, folder = 'run1'):
 
 		accuracy, precision, recall, f1 = nn.evaluate(X_placeholder, y_placeholder, Theta1, bias1, Theta2, bias2)
 
-		cost_summary = tf.summary.scalar('cost', cost);
-		accuracy_summary = tf.summary.scalar('accuracy', accuracy);
-		precision_summary = tf.summary.scalar('precision', precision);
-		recall_summary = tf.summary.scalar('recall', recall);
-		f1_summary = tf.summary.scalar('f1', f1);
-		summaries = tf.summary.merge_all();
+		cost_summary = tf.summary.scalar('cost', cost)
+		accuracy_summary = tf.summary.scalar('accuracy', accuracy)
+		precision_summary = tf.summary.scalar('precision', precision)
+		recall_summary = tf.summary.scalar('recall', recall)
+		f1_summary = tf.summary.scalar('f1', f1)
+		summaries = tf.summary.merge_all()
 
-		sess = tf.Session(graph=graph);
+		sess = tf.Session(graph=graph)
 		saver = tf.train.Saver()
 		init = tf.global_variables_initializer()
 		sess.run(init)
 
 		writer = tf.summary.FileWriter('./tmp/logs/' + folder, sess.graph)
 
-		NUM_STEPS = 4000;
+		NUM_STEPS = 4000
 
 		for step in range(NUM_STEPS):
-			sess.run(optimize, feed_dict={X_placeholder: X, y_placeholder: y});
+			sess.run(optimize, feed_dict={X_placeholder: X, y_placeholder: y})
 			if (step > 0) and ((step + 1) % 10 == 0):
-				acc, prec, rec, f, summary = sess.run([accuracy, precision, recall, f1, summaries], feed_dict={X_placeholder: X_val, y_placeholder: y_val});
-				writer.add_summary(summary, step);
-				print('Step', step + 1, 'of', NUM_STEPS);
+				acc, prec, rec, f, summary = sess.run([accuracy, precision, recall, f1, summaries], feed_dict={X_placeholder: X_val, y_placeholder: y_val})
+				writer.add_summary(summary, step)
+				print('Step', step + 1, 'of', NUM_STEPS)
 
 		save_path = saver.save(sess, './tmp/model_' + folder + '.ckpt')
 		print("Model saved in file: %s" % save_path)
-		sess.close();
+		sess.close()
