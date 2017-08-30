26c76be60cbf0c241d073505d5ecab622bc48b8d
Rob Allsopp <rca06d@my.fsu.edu>
2017-08-29 21:56:12 -0600

    support restoring from a checkpoint

--- train/task.py
+++ train/task.py
@@ -15,7 +15,9 @@ parser.add_argument('layers',
 										help="Specify nn hidden layer architecture. Provide space separated integers to specify the number of neurons in each hidden layer.",
 										nargs='*',
 										type=int)
-parser.add_argument('-s', '--steps', type=int, help="Specify number of training iterations.", default=4000)
+parser.add_argument('-n', '--num-iters', type=int, help="Specify number of training iterations.", default=4000)
+parser.add_argument('-s', '--start-iter', type=int, help="Specify the training iteration to start on.", default=0)
+parser.add_argument('-c', '--from-checkpoint', type=str, help="Specify a checkpoint to restore from.")
 
 def every_n_steps(n, step, callback):
 	if (step > 0) and ((step + 1) % n == 0):
@@ -43,7 +45,7 @@ class TrainerGraph:
 		return tf.summary.merge(summaries)
 
 
-def train(layers, dataset, num_steps=4000):
+def train(layers, dataset, num_steps=4000, restore_variables_from="", step_start=0):
 	run_name = '_'.join(map(str, layers)) + ' - ' + time.strftime('%Y-%m-%d_%H-%M-%S')
 	dataset.shuffle()
 
@@ -77,6 +79,10 @@ def train(layers, dataset, num_steps=4000):
 				graph_val.y_init: y_val
 			})
 
+			# must come after sess.run(init) or the restored vars will be wiped out
+			if restore_variables_from != "":
+				session_saver.restore(sess, restore_variables_from)
+
 			def add_summary(step):
 				train_results, val_results = sess.run([summaries_train, summaries_val])
 				summary_writer.add_summary(train_results, step)
@@ -88,7 +94,7 @@ def train(layers, dataset, num_steps=4000):
 				save_path = session_saver.save(sess, save_path, global_step=step, write_meta_graph=write_meta_graph)
 				logging.info("Model saved at: %s" % save_path)
 
-			for step in range(num_steps):
+			for step in range(step_start, num_steps):
 				sess.run(optimize)
 				every_n_steps(10, step, add_summary)
 				every_n_steps(100, step, save_checkpoint)
@@ -105,4 +111,4 @@ logging.info('Files loaded successfully. Loaded ' + str(dataset.num_examples) + 
 layers = [dataset.num_features] + args.layers + [dataset.num_labels]
 logging.info('Training neural network with architecture ' + ', '.join(map(str, layers)) + '...')
 
-train(layers, dataset, num_steps=args.steps)
+train(layers, dataset, num_steps=args.num_iters, restore_variables_from=args.from_checkpoint, step_start=args.start_iter)
