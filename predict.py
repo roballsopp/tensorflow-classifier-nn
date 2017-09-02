7be435621a6e86494368bebe2e3fc729211c9986
Rob Allsopp <rca06d@my.fsu.edu>
2017-09-02 09:18:03 -0600

    refactor prediction for autoencoder

--- predict.py
+++ predict.py
@@ -17,32 +17,25 @@ parser.add_argument('layers',
 parser.add_argument('-s', '--skip', type=int, help="Specify how many samples between each window.", default=50)
 
 
-def slider(layers, x, model_path, skip=50):
-	input_layer_size = layers[0]
+def predict(layers, x, model_path, overlap=1000):
+	num_features = layers[0]
 	num_labels = layers[-1]
-	slice_size = ((x.size // input_layer_size) * input_layer_size) - input_layer_size
 
 	graph = tf.Graph()
 	with graph.as_default():
 
 		tf_x = tf.placeholder(tf.float32, name='X')
 		net = nn.FullyConnected(layers)
-
-		output_acc = []
-
-		for i in range(0, input_layer_size, skip):
-			tf_x_shifted = tf.slice(tf_x, [i], [slice_size])
-			tf_x_matrix = tf.reshape(tf_x_shifted, [-1, input_layer_size])
-			prediction = net.forward_prop(tf_x_matrix)
-			output_acc.append(prediction)
-
-		output = tf.reshape(tf.concat(output_acc, 1), [-1, num_labels])
+		inputs = build_inputs(tf_x, num_features, overlap)
+		outputs = net.forward_prop(inputs)
+		
+		# TODO: upsample outputs and reshape back into 1 dimensional array
 
 		sess = tf.Session(graph=graph)
 		saver = net.get_saver()
 		saver.restore(sess, model_path)
 
-		predictions = sess.run(output, feed_dict={tf_x: x})
+		predictions = sess.run(outputs, feed_dict={tf_x: x})
 		sess.close()
 
 		return predictions
@@ -57,9 +50,30 @@ X = loadData.loadWav(args.input)
 logging.info('Wav loaded')
 
 logging.info('Predicting...')
-predictions = slider(layers, X, './tmp/1764_100_50_1 - 2017-02-6_23-13-15/model', args.skip)
+predictions = predict(layers, X, './tmp/1764_100_50_1 - 2017-02-6_23-13-15/model', args.skip)
 logging.info('Prediction complete.')
 
 logging.info('Saving predictions...')
 loadData.saveWav('./test_data/435_bounced.wav', np.amax(predictions, axis=1))
 logging.info('Predictions saved!')
+
+# take a plain array (x) and convert it into slices of size num_features, possibly overlapping them
+def build_inputs(x, num_features, overlap):
+	x_length = x.shape.as_list()[0]
+	num_features = tf.constant(num_features, name="num_features")
+	overlap = tf.constant(overlap, name="overlap")
+	base_size = num_features - overlap
+	remainder = x_length % base_size
+
+	def pad_input():
+		input_padding = tf.zeros([base_size - remainder], name="input_padding")
+		return tf.concat([x, input_padding], 0, name="append_input_padding")
+
+	x = tf.cond(remainder > 0, pad_input, lambda: x)
+	x = tf.reshape(x, [-1, base_size])
+
+	overlap_rows = x[1:, 0:overlap]
+	overlap_padding = tf.zeros([1, overlap], name="overlap_padding")
+	overlap_rows = tf.concat([overlap_rows, overlap_padding], 0, name="append_overlap_padding")
+
+	return tf.concat([x, overlap_rows], 1, name="create_final_inputs")
