6a44637b127c2e713355afe1710f71ca245ad1ff
Rob Allsopp <rca06d@my.fsu.edu>
2017-09-23 08:32:40 -0600

    model tweaks

--- train/model.py
+++ train/model.py
@@ -5,47 +5,37 @@ class Model:
 		layer1_out = tf.layers.conv1d(
 			inputs,
 			filters=64,
-			kernel_size=32,  # if input sample rate is 11025, 25 samples is ~2ms
-			strides=1,
+			kernel_size=16,  # if input sample rate is 11025, 25 samples is ~2ms
 			padding='same',
 			activation=tf.nn.relu,
 			name='hidden_layer_1',
 			reuse=reuse
 		)
 
-		# pool1_out = tf.layers.max_pooling1d(layer1_out, 25, 1, padding='same', name='pooling_layer_1')  # pools the highest filter values over the next 50 samples
-
 		layer2_out = tf.layers.conv1d(
 			layer1_out,
 			filters=20,
-			kernel_size=128,
-			strides=1,
+			kernel_size=64,
 			padding='same',
 			activation=tf.nn.relu,
 			name='hidden_layer_2',
 			reuse=reuse
 		)
 
-		pool2_out = tf.layers.max_pooling1d(layer2_out, 25, 1, padding='same', name='pooling_layer_2')
-
 		layer3_out = tf.layers.conv1d(
-			pool2_out,
+			layer2_out,
 			filters=5,
-			kernel_size=256,
-			strides=1,
+			kernel_size=64,
 			padding='same',
 			activation=tf.nn.relu,
 			name='hidden_layer_3',
 			reuse=reuse
 		)
 
-		pool3_out = tf.layers.max_pooling1d(layer3_out, 25, 1, padding='same', name='pooling_layer_3')
-
 		final_out = tf.layers.conv1d(
-			pool3_out,
+			layer3_out,
 			filters=1,
-			kernel_size=150,  # use 6 pooled frames to determine if this is a hit or not
-			strides=1,
+			kernel_size=100,
 			padding='same',
 			name='output_layer',
 			reuse=reuse
@@ -56,7 +46,7 @@ class Model:
 	def forward_prop(self):
 		return tf.nn.sigmoid(self._raw_outputs)
 
-	def loss(self, correct_labels, positive_weight=1600):
+	def loss(self, correct_labels, positive_weight=1000):
 		batch_size = tf.cast(tf.shape(correct_labels)[0], tf.float32)
 
 		negative_mask = tf.abs(correct_labels - 1)
