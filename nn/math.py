0596f48986efc81b0c8f2671c707266121182d78
rob <rca06d@my.fsu.edu>
2017-12-09 10:10:46 -0700

    fix reduce bug

--- nn/math.py
+++ nn/math.py
@@ -2,19 +2,19 @@ import tensorflow as tf
 
 def rms_normalize_per_band(spectrogram, channels_last=True):
 	band_axis = 1 if channels_last else 2
-	band_rms = tf.sqrt(tf.reduce_mean(tf.square(spectrogram), axis=[band_axis]))
+	band_rms = tf.sqrt(tf.reduce_mean(tf.square(spectrogram), keep_dims=True, axis=[band_axis]))
 	den = tf.expand_dims(band_rms, axis=1)
 
 	return spectrogram / den
 
 
 def rms_normalize(inputs, axis=None):
-	rms = tf.sqrt(tf.reduce_mean(tf.square(inputs), axis=axis))
+	rms = tf.sqrt(tf.reduce_mean(tf.square(inputs), keep_dims=True, axis=axis))
 	return inputs / rms
 
 
 def normalize(inputs, axis=None):
-	maximum = tf.reduce_max(tf.abs(inputs), axis=axis)
+	maximum = tf.reduce_max(tf.abs(inputs), keep_dims=True, axis=axis)
 	return inputs / maximum
 
 
