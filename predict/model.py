e9e547fe8a95658c6fc9c18ff2720f62205fd42f
rob <rca06d@my.fsu.edu>
2017-12-21 18:57:32 -0700

    handle channels first for padding op

--- predict/model.py
+++ predict/model.py
@@ -52,39 +52,45 @@ def pre_process(inputs, channels_last=True):
 
 def post_process(outputs, channels_last=True):
 	time_axis = 0 if channels_last else 1
+	# everything exits with an rms of 1
 	return nn.rms_normalize(outputs, axis=time_axis, non_zero=True)
 
 
 def magnitude_model(inputs, channels_last=True):
 	normed_inputs = pre_process(inputs, channels_last=channels_last)
+
+	time_axis = 0 if channels_last else 1
+	band_axis = 1 if channels_last else 2
+
 	# TODO: play with fft size (trade frequency reso for time reso),
 	# window size (Odd window will make phase behave better (STFT class 2)),
 	# and phase unwrapping (looks like phase spectrogram would be more useful for determining transients when unwrapped).
 	# Different windows might work better for noise rejection (blackman/blackman-harris)
 	fft_size = 256
-	# TODO: handle channels first
-	fft_inputs = tf.pad(normed_inputs, [[fft_size - 1, 0], [0, 0]])
+
+	# shift input to fft so output energy is positioned correctly for later stages
+	# if we don't shift here, the fft draws energy forward in time, making our markers early when they come out later
+	padding = [[0, 0], [0, 0]]
+	padding[time_axis] = [fft_size - 1, 0]
+	fft_inputs = tf.pad(normed_inputs, padding)
+
 	# advantage of using fft to generate magnitude over just raw signal is the fft magnitude is separated from the phase component
 	# in the raw signal, the magnitudes are all there, but the sine waves are shifted to make them very uneven
 	stfts = nn.stft(fft_inputs, fft_length=fft_size, step=1, channels_last=channels_last)
 	sig_mag = tf.abs(stfts)
-
-	# time_axis = 0 if channels_last else 1
 
 	# normalize each band. reduce along time axis to do this. very similar effect to just dropping all low freqs
 	# sig_mag = nn.mean_normalize(sig_mag, axis=time_axis)
 	# drop all low freqs
 	# sig_mag = sig_mag[:, 15:, :]
 
-	band_axis = 1 if channels_last else 2
-
 	total_mag = tf.reduce_sum(sig_mag, axis=[band_axis])
-
-	# create the "batch" dim, even though there is only one example
-	total_mag = tf.expand_dims(total_mag, axis=0)
 
 	# the log scale really brings out the lower transients, it must also bring out the noise, but it seems pretty dang good...
 	total_mag_log = tf.log(total_mag + 1e-16)
+
+	# create the "batch" dim, even though there is only one example
+	total_mag_log = tf.expand_dims(total_mag_log, axis=0)
 
 	mag_diff = tf.nn.convolution(
 		total_mag_log,
